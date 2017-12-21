fa19e5009137beaa1c5e3bf29919a2a19dc04423
CARRASCO\pgonzalez <pablo.gonzalez@uruit.com>
2017-12-21 16:48:20 -0300

    Activity indicator on login page

---
+++ TuPedido/Extensions/Converters/NegateConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TuPedido.Extensions
+{
+    public class NegateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+            {
+                throw new InvalidOperationException("The target must be a boolean");
+            }
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+    }
+}
