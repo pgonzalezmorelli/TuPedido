c6618a0839dff6c83126f2b67415336fe2b47b5e
CARRASCO\pgonzalez <pablo.gonzalez@uruit.com>
2018-01-06 16:35:18 -0300

    Orders list UI fixes

---
+++ TuPedido/Extensions/Converters/NullableToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TuPedido.Extensions
+{
+    public class NullableToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var unavailable = "No disponible";
+
+            if (value == null)
+                return "No disponible";
+
+            if (value is DateTime?)
+                return ((DateTime?)value).HasValue ? new DateToStringConverter().Convert(value, targetType, parameter, culture) : unavailable;
+
+            if (value is int?)
+                return ((int?)value).HasValue ? $"{value.ToString()} minutos" : unavailable;
+
+            throw new InvalidOperationException("The target must be a datetime? or int?");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
