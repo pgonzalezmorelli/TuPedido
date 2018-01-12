9ea62e9b57da530200e34d4bf6b67417f0d47107
CARRASCO\pgonzalez <pablo.gonzalez@uruit.com>
2018-01-12 14:46:25 -0300

    Logo and navigation bar refresh

--- TuPedido/Extensions/PageExtensions.cs
+++ TuPedido/Extensions/PageExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using TuPedido;
 using TuPedido.Managers;
 using TuPedido.Services;
@@ -15,16 +17,17 @@ namespace Xamarin.Forms
             }
             else
             {
+                var userNames = App.CurrentUser.Name.Split(new char[] { ' ' });
+                var displayName = (userNames.Length > 2 ? $"{userNames[0]} {userNames[1]}" : userNames[0]) + $" {userNames.Last().Substring(0,1)}.";
+                NavigationPage.SetTitleIcon(page, "logo.png");
+
+                Enumerable.Range(1, 2).ForEach(i => page.ToolbarItems.Add(new ToolbarItem()));
+                page.ToolbarItems.Add(new ToolbarItem { Text = displayName });
                 page.ToolbarItems.Add(new ToolbarItem
                 {
-                    Icon = "logo.png"
-                });
-                page.ToolbarItems.Add(new ToolbarItem
-                {
-                    Text = "Logout",
+                    Icon = "logout.png",
                     Command = new Command(async () => await Logout())
                 });
-                page.Title = App.CurrentUser.Name;
             }
         }
 
